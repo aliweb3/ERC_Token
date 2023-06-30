393be4556f19aec5990c40ccd16cdc1ca68e2efc
aliweb3 <114404873+aliweb3@users.noreply.github.com>
2023-06-30 15:19:32 +0330

    transfer function

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -22,8 +22,11 @@ function balanceOf(address _owner) public view returns (uint256 ){
  return s_balances[_owner];
 }
 
-function transfer(address _to, uint256 _value) public returns (bool success){
-    
+function transfer(address _to, uint256 _value) public  {
+ uint256 previusBalances = balanceOf(msg.sender) + balanceOf(_to) ;
+ s_balances[msg.sender] -= _value ;
+ s_balances[_to] += _value;
+ require(previusBalances == balanceOf(msg.sender) + balanceOf(_to));
 }
 
 
